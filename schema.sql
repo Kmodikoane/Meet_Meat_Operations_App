ebcf5f49978e2814e63b813076ca87ca0fd185e2
Kgomotso Modikoane <kgomotso.it.modikoane@gmail.com>
2026-08-07 20:30:08 +0200

    Refactor: fixed first, last name and role in users table

--- schema.sql
+++ schema.sql
@@ -3,7 +3,17 @@ CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT NOT NULL UNIQUE,
     email TEXT NOT NULL
+      -- Enforce required First Name (No empty strings allowed)
+    first_name TEXT NOT NULL CHECK(length(trim(first_name)) > 0),
+    
+    -- Enforce required Surname (No empty strings allowed)
+    last_name TEXT NOT NULL CHECK(length(trim(last_name)) > 0),
+    role TEXT NOT NULL CHECK(role IN ('admin', 'exec'))
 );
 
+
 -- 2. Seed the database with the first 2 users
-INSERT OR IGNORE INTO users (username, email) VALUES ('Kgomotso', 'motsomodikoane@gmail.com');
+INSERT OR IGNORE INTO users (username, email, first_name, last_name, role) VALUES ('KModikoane', 'motsomodikoane@gmail.com', 'Kgomotso', 'Modikoane', 'admin');
+
+INSERT INTO users (username, email, first_name, last_name, role) 
+VALUES ('Kgothatso@MeetMeat', 'kgomotso.it.modikoane@gmail.com', 'Kgothatso', 'MeetMeat','Executive', 'exec');
