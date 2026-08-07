7eea59a2957819d1492b97e6f069aa192869d9bd
Kgomotso Modikoane <kgomotso.it.modikoane@gmail.com>
2026-08-07 23:29:38 +0200

    Feature: Created security token variable for credentials authorisation

--- schema.sql
+++ schema.sql
@@ -2,18 +2,21 @@
 CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT NOT NULL UNIQUE,
-    email TEXT NOT NULL
+    email TEXT NOT NULL,
+    
       -- Enforce required First Name (No empty strings allowed)
     first_name TEXT NOT NULL CHECK(length(trim(first_name)) > 0),
     
     -- Enforce required Surname (No empty strings allowed)
     last_name TEXT NOT NULL CHECK(length(trim(last_name)) > 0),
-    role TEXT NOT NULL CHECK(role IN ('admin', 'exec'))
+    role TEXT NOT NULL CHECK(role IN ('admin', 'exec')),
+    token TEXT NOT NULL UNIQUE
 );
 
 
 -- 2. Seed the database with the first 2 users
-INSERT OR IGNORE INTO users (username, email, first_name, last_name, role) VALUES ('KModikoane', 'motsomodikoane@gmail.com', 'Kgomotso', 'Modikoane', 'admin');
+INSERT OR IGNORE INTO users (username, email, first_name, last_name, role, token) VALUES ('KModikoane', 'motsomodikoane@gmail.com', 'Kgomotso', 'Modikoane', 'admin', 'TKN_placeholder1');
 
-INSERT INTO users (username, email, first_name, last_name, role) 
-VALUES ('Kgothatso@MeetMeat', 'kgomotso.it.modikoane@gmail.com', 'Kgothatso', 'MeetMeat','Executive', 'exec');
+INSERT INTO users (username, email, first_name, last_name, role, token) 
+VALUES ('Kgothatso@MeetMeat', 'kgomotso.it.modikoane@gmail.com', 'Kgothatso', 'MeetMeat','Executive', 'exec','TKN_placeholder1')
+;
