667b341ffd9983bed545f48cbccb0599bc7e1219
Kgomotso Modikoane <kgomotso.it.modikoane@gmail.com>
2026-08-11 13:36:24 +0200

    Refactor: removed hardcoded token to use a dynamic token generator

--- schema.sql
+++ schema.sql
@@ -14,10 +14,10 @@ CREATE TABLE IF NOT EXISTS users (
 );
 
 
--- 2. Seed the database with the first 2 users
-INSERT OR IGNORE INTO users ( username, email, first_name, last_name, role, token) 
-VALUES ('KModikoane', 'motsomodikoane@gmail.com', 'Kgomotso', 'Modikoane', 'admin', 'TKN_placeholder1');
+-- -- 2. Seed the database with the first 2 users
+-- INSERT OR IGNORE INTO users ( username, email, first_name, last_name, role) 
+-- VALUES ('KModikoane', 'motsomodikoane@gmail.com', 'Kgomotso', 'Modikoane', 'admin');
 
-INSERT OR IGNORE INTO users (username, email, first_name, last_name, role, token) 
-VALUES ('Kgothatso@MeetMeat', 'kgomotso.it.modikoane@gmail.com', 'Kgothatso', 'MeetMeat', 'exec','TKN_placeholder2')
-;
+-- INSERT OR IGNORE INTO users (username, email, first_name, last_name, role) 
+-- VALUES ('Kgothatso@MeetMeat', 'kgomotso.it.modikoane@gmail.com', 'Kgothatso', 'MeetMeat', 'exec')
+-- ;
