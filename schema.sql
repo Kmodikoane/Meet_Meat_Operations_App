3616dcd474d4204f8918546ec40acb23bd8cea3c
Kgomotso Modikoane <kgomotso.it.modikoane@gmail.com>
2026-08-11 14:17:49 +0200

    Refactor: Added phone number variable for security feature

--- schema.sql
+++ schema.sql
@@ -1,18 +1,13 @@
--- 1. Create the users table
 CREATE TABLE IF NOT EXISTS users (
-    user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT NOT NULL UNIQUE,
-    email TEXT NOT NULL,
-    
-      -- Enforce required First Name (No empty strings allowed)
+    email TEXT NOT NULL UNIQUE,
+    phone_number TEXT,
     first_name TEXT NOT NULL CHECK(length(trim(first_name)) > 0),
-    
-    -- Enforce required Surname (No empty strings allowed)
     last_name TEXT NOT NULL CHECK(length(trim(last_name)) > 0),
     role TEXT NOT NULL CHECK(role IN ('admin', 'exec')),
     token TEXT NOT NULL UNIQUE
 );
-
 
 -- -- 2. Seed the database with the first 2 users
 -- INSERT OR IGNORE INTO users ( username, email, first_name, last_name, role) 
